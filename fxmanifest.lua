2dd67ed42da32f684d586167beb854930068a544
PRXStore <azsx9dcfv@gmail.com>
2026-05-20 16:55:51 +0300

    .

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version "adamant"
+game "gta5"
+
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'handling.meta',
+    'carcols.meta',
+    'weaponarchetypes.meta',
+    'contentunlocks.meta',
+    'dlctext.meta',
+    'explosion.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+data_file 'EXPLOSION_INFO_FILE' 'explosion.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'contentunlocks.meta'
+
